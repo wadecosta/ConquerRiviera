4b134be7ce91de1bed1a70a665a67a5a3abb3e41
LuiM112 <89751770+LuiM112@users.noreply.github.com>
2022-04-27 22:04:59 -0700

    Model Animations
    Got to fixed the animations a bit

--- Assets/Character/Controls.cs
+++ Assets/Character/Controls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,61 +6,107 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     public float runForce = 50f;
-        public float jumpForce = 20f;
-        public float jumpBonus = 6f;
-        public float runBonus = 5f;
+    public float jumpForce = 20f;
+    public float jumpBonus = 6f;
+    public float runBonus = 5f;
+
+    public float maxRunSpeed = 6f;
+
+    public bool feetInContactWithGround;
     
-        public float maxRunSpeed = 6f;
+
+    private Collider collider;
+
+    private Animator animComp;
     
-        public bool feetInContactWithGround;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+        animComp = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bounds bounds = GetComponent<Collider>().bounds;
+        feetInContactWithGround = Physics.Raycast(transform.position, Vector3.down, bounds.extents.y + 0.1f);
+
+        float axis = Input.GetAxis("Horizontal");
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(Vector3.right * axis * runForce, ForceMode.Force);
+
+        if (feetInContactWithGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
         
-    
-        private Collider collider;
-
-
-        // Start is called before the first frame update
-        void Start()
+        else if (body.velocity.y > 0f &&  Input.GetKey(KeyCode.Space))
         {
-            collider = GetComponent<Collider>();
+            body.AddForce(Vector3.up * jumpBonus, ForceMode.Force);
         }
-    
-        // Update is called once per frame
-        void Update()
+
+        float xVelocity = Mathf.Clamp(body.velocity.x, -maxRunSpeed, maxRunSpeed);
+
+        if (Mathf.Abs(axis) < 0.1f)
         {
-            Bounds bounds = GetComponent<Collider>().bounds;
-            feetInContactWithGround = Physics.Raycast(transform.position, Vector3.down, bounds.extents.y + 0.1f);
-    
-            float axis = Input.GetAxis("Horizontal");
-            Rigidbody body = GetComponent<Rigidbody>();
-            body.AddForce(Vector3.right * axis * runForce, ForceMode.Force);
-    
-            if (feetInContactWithGround && Input.GetKeyDown(KeyCode.Space))
-            {
-                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            }
+            xVelocity *= 0.9f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            maxRunSpeed += 7f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            maxRunSpeed -= 7f;
+        }
+
+        body.velocity = new Vector3(xVelocity, body.velocity.y, body.velocity.z);
+        
+        animComp.SetFloat("Speed", body.velocity.magnitude);
+        
+        if (body.velocity.y < 0)
+        {
+            animComp.SetFloat("FallSpeed", body.velocity.y);
+        }
+
+        else
+        {
+            animComp.SetFloat("FallSpeed", 0);
+        }
+
+        if (body.velocity.y > 0)
+        {
+            animComp.SetFloat("JumpSpeed", body.velocity.y);
+        }
+
+        else
+        {
+            animComp.SetFloat("JumpSpeed", 0);
+        }
+
+        if (feetInContactWithGround == true)
+        {
+            animComp.SetBool("TouchingGround", true);
+        }
+
+        else
+        {
+            animComp.SetBool("TouchingGround", false);
+        }
+        
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animComp.SetBool("Attacking", true);
             
-            else if (body.velocity.y > 0f &&  Input.GetKey(KeyCode.Space))
-            {
-                body.AddForce(Vector3.up * jumpBonus, ForceMode.Force);
-            }
-    
-            float xVelocity = Mathf.Clamp(body.velocity.x, -maxRunSpeed, maxRunSpeed);
-    
-            if (Mathf.Abs(axis) < 0.1f)
-            {
-                xVelocity *= 0.9f;
-            }
-    
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                maxRunSpeed += 7f;
-            }
-    
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                maxRunSpeed -= 7f;
-            }
-    
-            body.velocity = new Vector3(xVelocity, body.velocity.y, body.velocity.z);
         }
+        else
+        {
+            animComp.SetBool("Attacking", false);
+        }
+    }
 }
