f2ff60b64685d29c26a73e3dca928e10f1e5f8a4
MiguelTayag <mtayag@csumb.edu>
2022-05-04 18:00:01 -0700

    Mayan Mask
    Added:
    Heatwave SFX
    Fireball SFX
    Death SFX
    Player Detection for attack
    Health bar UI

--- Mayan Mask/Assets/Scripts/MaskMovement.cs
+++ Mayan Mask/Assets/Scripts/MaskMovement.cs
@@ -3,12 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
+
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class MaskMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject fireballSFX;
+    public GameObject heatwaveSFX;
+    public GameObject deathScreamSFX;
+
+    public GameObject healthBarUI;
+    public Slider slider;
+    
     public GameObject mask;
     public GameObject player;
 
@@ -19,10 +28,13 @@ public class MaskMovement : MonoBehaviour
     public Vector3 origin;
     public Vector3 direction;
     public Vector3 wirePosition;
+    private Vector3 newPosition;
     
     public float elapsedTime = 0f;
+    public float countDown = 5f;
 
-    private int health;
+    public float health;
+    private float maxHealth;
 
     public GameObject imgIndicator;
     public GameObject hwIndicator;
@@ -38,11 +50,20 @@ public class MaskMovement : MonoBehaviour
     { 
         sphereRadius = 5;
         health = 100;
+        countDown = 5f;
+        maxHealth = 100f;
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        slider.value = CalculateHealth();
+
+        if (health < maxHealth)
+        {
+            healthBarUI.SetActive(true);
+        }
         int indicator = 2;
         elapsedTime += Time.deltaTime;
         if (elapsedTime >= 7f)
@@ -51,36 +72,63 @@ public class MaskMovement : MonoBehaviour
             indicator = choseIndicator();
             StartCoroutine(chooseAttack(indicator));
         }
+        countDown -= Time.deltaTime;
+
+        if (countDown > 0)
+        {
+            mask.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        else if (countDown > -5.0f)
+        {
+            mask.transform.position += Vector3.back * Time.deltaTime;
+        }
+        else
+        {
+            countDown = 5f;
+        }
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(new Vector3((float) (mask.transform.position.x + 0.67), 
-            (float) (mask.transform.position.y - 0.66),
-            (float) (mask.transform.position.z -4.27)), sphereRadius);
-    }
+    // private void OnDrawGizmosSelected()
+    // {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireSphere(new Vector3((float) (mask.transform.position.x + 0.67), 
+    //         (float) (mask.transform.position.y - 0.66),
+    //         (float) (mask.transform.position.z -4.27)), sphereRadius);
+    // }
 
     private void heatWave()
     {
+        if (health < 0)
+        {
+            die();
+        }
         imgIndicator.SetActive(false);
         heatWaveParticles.Play();
+        heatwaveSFX.SetActive(true);
         ParticleSystem.EmissionModule em = heatWaveParticles.emission;
         em.enabled = true;
         origin = mask.transform.position;
         direction = mask.transform.forward;
         RaycastHit hit;
-        if (Physics.SphereCast(origin, sphereRadius, direction, out hit, maxDistance, layerMask,
-                QueryTriggerInteraction.UseGlobal))
+        if (Physics.Raycast(origin, mask.transform.TransformDirection(Vector3.forward), out hit, 5f))
         {
-            Debug.Log(hit.transform.gameObject);
+            Debug.DrawRay(origin, mask.transform.TransformDirection(Vector3.forward) * 5f, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(origin, mask.transform.TransformDirection(Vector3.forward) * 5f, Color.white);
+            Debug.Log("Did not Hit");
         }
     }
 
     private void fireball()
     {
         imgIndicator.SetActive(false);
+        fireballSFX.SetActive(true);
         Instantiate(fireballPrefab, fireballSpawner.position, GameObject.Find("Fireball Spawner").transform.rotation);
+
+
     }
 
     private IEnumerator chooseAttack(int i)
@@ -103,8 +151,9 @@ public class MaskMovement : MonoBehaviour
 
     private int choseIndicator()
     {
+        fireballSFX.SetActive(false);
+        heatwaveSFX.SetActive(false);
         int i = Random.Range(0, 2);
-        Debug.Log("I picked " + i);
         if (i == 0)
         {
             imgIndicator = hwIndicator;
@@ -116,5 +165,16 @@ public class MaskMovement : MonoBehaviour
             imgIndicator.SetActive(true);
         }
         return i;
+    }
+
+    private void die()
+    {
+        deathScreamSFX.SetActive(true);
+        Destroy(this);
+    }
+    
+    float CalculateHealth()
+    {
+        return (health / maxHealth);
     }
 }
